6dbcc026763b567a4e2849b4e9b85d85252c4518
JohnOnGit <fraga_jonathan@protonmail.ch>
2022-04-17 14:08:43 +0200

    Update test_wifi.py

--- test_wifi.py
+++ test_wifi.py
@@ -15,10 +15,11 @@ print("wifi list:")
 lis = N1.scan()
 for q in lis:
     print(q)
- 
+# https://docs.micropython.org/en/v1.9.3/wipy/library/network.html
 N1.connect("password","ssid")
 if N1.isconnected():
     print("    ip               gateway           netmask            MAC            ssid")
+    N1.ifconfig(config=('dhcp'))
     print(N1.ifconfig())
     s=usocket.socket()
     addr=('192.168.1.23',9999)
