cc189b4656fbadf39aa5959ff46236c3a344651f
keokod <keo.n@free.fr>
2022-04-17 05:58:44 +0200

    Create test_wifi.py

---
+++ test_wifi.py
@@ -0,0 +1,26 @@
+#https://wiki.seeedstudio.com/LAN_Communications/
+
+import network
+import usocket
+from machine import Pin, I2C, ADC, UART, SPI, PWM
+from time import sleep
+ 
+N1 = network.WLAN_SPI(network.STA_IF)
+N1.active(True)
+ 
+print("API list:")
+dir(N1)
+ 
+print("wifi list:")
+lis = N1.scan()
+for q in lis:
+    print(q)
+ 
+N1.connect("password","ssid")
+if N1.isconnected():
+    print("    ip               gateway           netmask            MAC            ssid")
+    print(N1.ifconfig())
+    s=usocket.socket()
+    addr=('192.168.1.23',9999)
+    s.connect(addr)
+    s.send('Hello! Wio RP2040')
