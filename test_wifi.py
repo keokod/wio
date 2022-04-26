4141c31742d66d871a27b90b5c0ddd3dc5c83980
keokod <keo.n@free.fr>
2022-04-26 15:54:18 +0200

    Update test_wifi.py

--- test_wifi.py
+++ test_wifi.py
@@ -1,5 +1,3 @@
-#https://wiki.seeedstudio.com/LAN_Communications/
-
 import network
 import usocket
 from machine import Pin, I2C, ADC, UART, SPI, PWM
@@ -7,7 +5,8 @@ from time import sleep
  
 N1 = network.WLAN_SPI(network.STA_IF)
 N1.active(True)
- 
+print(N1.ifconfig())
+
 print("API list:")
 dir(N1)
  
@@ -16,11 +15,11 @@ lis = N1.scan()
 for q in lis:
     print(q)
  
-N1.connect("password","ssid")
+N1.connect("name_ssid_ohkod","key_____")
 if N1.isconnected():
     print("    ip               gateway           netmask            MAC            ssid")
     print(N1.ifconfig())
     s=usocket.socket()
-    addr=('192.168.1.23',9999)
+    addr=('192.168.1.46',9999)
     s.connect(addr)
-    s.send('Hello! Wio RP2040')
+    s.send("wio avec ohkod")
